f52f04fe59df4207348eceb88cf4067eec5e128d
kapil484 <kapil.kr909@gmail.com>
2021-07-11 12:35:43 +0530

    add user model

---
+++ Peacher/ideapeacher/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+
+
+class ideapeacher(models.Model):
+    user = models.OneToOneField(User, null= True, on_delete= models.CASCADE)
+    name = models.CharField(max_length=200, null= True)
